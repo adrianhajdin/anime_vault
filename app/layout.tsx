4c91e5143f60ae02691580ad49f39b285ed006f0
AbdallahAyed <abdalla.ayed.2010@gmail.com>
2024-02-09 02:31:25 -0800

    Add intercepting routes functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 
-import Hero from "@/components/Hero";
-import Footer from "@/components/Footer";
-
 import "./globals.css";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
@@ -15,14 +12,18 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  modal,
 }: {
   children: React.ReactNode;
+  modal: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={dmSans.className}>
         <main className="max-w-7xl mx-auto bg-[#0F1117] min-h-screen">
           {children}
+          {modal}
+          <div id="modal-root" />
         </main>
       </body>
     </html>
