d1006be35d85c110aa0ec085ba704248aabd17af
AbdallahAyed <abdalla.ayed.2010@gmail.com>
2024-02-09 02:52:42 -0800

    Fix "use client" issue

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
@@ -12,7 +14,7 @@ function BackButton() {
       width={40}
       height={40}
       alt="Back"
-      className="p-2"
+      className="p-2 cursor-pointer"
       onClick={() => router.back()}
     />
   );
