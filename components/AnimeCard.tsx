15e4ef9ad05e3a1ab3a57e64a9db819db57a8e67
Patrik <patriklevak@gmail.com>
2024-03-02 21:38:27 +0100

    first push

--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { MotionDiv } from "./MotionDiv";
 
 export interface AnimeProp {
   id: string;
@@ -17,12 +18,28 @@ interface Prop {
   index: number;
 }
 
-function AnimeCard({ anime }: Prop) {
+const variants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+};
+
+function AnimeCard({ anime, index }: Prop) {
   return (
-    <div className="max-w-sm rounded relative w-full">
+    <MotionDiv
+      variants={variants}
+      initial="hidden"
+      animate="visible"
+      transition={{
+        delay: index * 0.1,
+        ease: "easeInOut",
+        duration: 0.5,
+      }}
+      viewport={{ amount: 0.5 }}
+      className="max-w-sm rounded relative w-full"
+    >
       <div className="relative w-full h-[37vh]">
         <Image
-          src={anime.image.original}
+          src={`https://shikimori.one/${anime.image.original}`}
           alt={anime.name}
           fill
           className="rounded-xl"
@@ -64,7 +81,7 @@ function AnimeCard({ anime }: Prop) {
           </div>
         </div>
       </div>
-    </div>
+    </MotionDiv>
   );
 }
 
--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,32 @@
+"use client";
+
 import Image from "next/image";
+import { useInView } from "react-intersection-observer";
+import { useEffect, useState } from "react";
+import { fetchAnime } from "@/app/action";
+
+let page = 2;
 
 function LoadMore() {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<JSX.Element[]>([]);
+
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(page).then((res) => {
+        setData((prevData) => [...prevData, ...res]);
+        page++;
+      });
+    }
+  }, [inView]);
+
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data}
+      </section>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
